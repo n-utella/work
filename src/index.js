f2ee8714bcd80f51a32866df1928290598f1187b
EC2 Default User <ec2-user@ip-172-31-10-118.ap-northeast-2.compute.internal>
2018-08-04 14:25:32 +0000

    Init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+ReactDOM.render(
+  <h1>Hey Buddy</h1>,
+  document.getElementById('app'),
+);
+if (module.hot) {
+  module.hot.accept();
+}
+
